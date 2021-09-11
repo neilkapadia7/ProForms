a0a153712d34776e85ee09441271170c996b6f55
Neil Kapadia <neilkapadia7@gmail.com>
2021-09-11 20:27:21 +0530

    setup

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import "module-alias";
+import connectDB from './config/db.js'
 
 const app = express();
 app.use(express.json());
+// connectDB();
 
 let PORT = process.env.PORT || 5000;
 
