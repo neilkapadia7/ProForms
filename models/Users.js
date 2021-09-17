20215c9a70b6845b4c7811432f8d80a39dae9b60
Neil Kapadia <neilkapadia7@gmail.com>
2021-09-17 16:02:49 +0530

    routes added

--- models/Users.js
+++ models/Users.js
@@ -2,43 +2,43 @@ const mongoose = require('mongoose');
 
 const Users = new mongoose.Schema({
     name: {
-        type: String,
-        trim: true,
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+    },
+    password: { type: String },
+    mobile: {
+      type: String,
+      validate: {
+        validator: function (mobile) {
+        return /^[0-9]*$/.test(mobile);
       },
-      email: {
-        type: String,
-        trim: true,
-        lowercase: true,
+      message: (props) => `${props.value} is not a valid mobile number!`,
       },
-      password: { type: String },
-      mobile: {
-        type: String,
-        validate: {
-          validator: function (mobile) {
-            return /^[0-9]*$/.test(mobile);
-          },
-          message: (props) => `${props.value} is not a valid mobile number!`,
-        },
-      },
-      isDeleted: {
-        type: Boolean,
-        default: false,
-      },
-      referredBy: { type: String },
-      referralCode: { type: String },
-      signupType: {type: Number},  // 1 - phone, 2 - email, 3 - linkedin, 4 - google
-      userType: {type: Number},
-      photo: {type: String},
-      isMobileVerified: {type: Boolean},
-      isEmailVerified: {type: Boolean},
-      token: {type: String}
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    referredBy: { type: String },
+    referralCode: { type: String },
+    signupType: {type: Number},  // 1 - phone, 2 - email, 3 - linkedin, 4 - google
+    userType: {type: Number},
+    photo: {type: String},
+    isMobileVerified: {type: Boolean},
+    isEmailVerified: {type: Boolean},
+    token: {type: String}
 },{ 
     timestamps: {
-        createdAt: true,
-        updatedAt: true,
-      },
-      toJSON: { virtuals: true },
-      toObject: { virtuals: true },
+      createdAt: true,
+      updatedAt: true,
+    },
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
 });
 
 module.exports = mongoose.model("Users", Users);
