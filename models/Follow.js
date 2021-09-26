2f838556805f216a2c375cf057c1d18ca1a6984b
Neil Kapadia <neilkapadia7@gmail.com>
2021-09-26 16:37:52 +0530

    Followers Schema

---
+++ models/Follow.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const followSchema = new mongoose.Schema({
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'Users'},
+    followersCount: {type: Number},
+    followingCount: {type: Number},
+    followers: [{type: mongoose.Schema.Types.ObjectId, ref: 'Users'}],
+    followings: [{type: mongoose.Schema.Types.ObjectId, ref: 'Users'}],
+},{ 
+    timestamps: {
+      createdAt: true,
+      updatedAt: true,
+    },
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+});
+
+const Follows = mongoose.model('Follows', followSchema);
+
+module.exports = Follows;
